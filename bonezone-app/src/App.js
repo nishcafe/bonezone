cf34446972a4c80d01d3fb23621b0541560cecc5
hernpiblo <hpingleenet@gmail.com>
2021-07-23 23:00:11 +0800

    FE

--- bonezone-app/src/App.js
+++ bonezone-app/src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "./MainPage.css";
+import TableEntry from "./TableEntry";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div classNameName="App">
+      <div className="main-page">
+        <div className="header">
+          <div className="title">Some Header</div>
+          <div className="username">Mr John</div>
+          <button className="exit">Exit</button>
+        </div>
+        <hr />
+        <div className="main-section">
+          <div className="discussion">
+            <div className="title">Discussion</div>
+            <hr />
+            <div className="table">
+              <div className="table-header">
+                <div className="number-header">S/n</div>
+                <div className="question-header">Title</div>
+                <div className="author-header">Author</div>
+                <div className="time-header">Time</div>
+                <div className="status-header">Status</div>
+              </div>
+              
+              <TableEntry />
+              <TableEntry />
+              <TableEntry />
+              <TableEntry />
+            </div>
+          </div>
+          <div className="chat">
+            <div className="chat-header">Chat</div>
+            <hr />
+            <div className="chat-view">
+              <div className="messages">hello</div>
+              <input type="text" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ bonezone-app/src/TableEntry.js
@@ -0,0 +1,38 @@
+import React from "react";
+// import Accordion from "@material-ui/core/Accordion";
+// import AccordionSummary from "@material-ui/core/AccordionSummary";
+// import AccordionDetails from "@material-ui/core/AccordionDetails";
+// import Typography from "@material-ui/core/Typography";
+
+function TableEntry() {
+  return (
+    <div class="table-entry">
+      <div class="number">1</div>
+      <div class="question"></div>
+      <div class="author">Mr John</div>
+      <div class="time">10:30 am</div>
+      <div class="status">Unanswered</div>
+      {/* // <Accordion>
+      //   <AccordionSummary
+      //     aria-controls="panel1a-content"
+      //     id="panel1a-header"
+      //     className="table-entry"
+      //   >
+      //     <div className="number">1</div>
+      //     <div className="question">How to win hackathon</div>
+      //     <div className="author">Mr John</div>
+      //     <div className="time">10:30 am</div>
+      //     <div className="status">Unanswered</div>
+      //   </AccordionSummary>
+      //   <AccordionDetails>
+      //     <div>
+      //       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+      //       malesuada lacus ex, sit amet blandit leo lobortis eget.
+      //     </div>
+      //   </AccordionDetails>
+      // </Accordion> */}
+    </div>
+  );
+}
+
+export default TableEntry;
